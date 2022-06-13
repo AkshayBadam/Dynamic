e41b65e8ad97dacf0ad5efc48bf903e410cec627
Akshay Badam <Akshay_Badam@epam.com>
2022-06-13 22:51:05 +0530

    Add project files.

---
+++ Dynamic/Program.cs
@@ -0,0 +1,24 @@
+﻿//   dynamic d= "Akshay";
+//  Console.WriteLine(d);
+//    d= 38;
+//   Console.WriteLine(d);
+//    d = 44.95;
+//    System.Console.WriteLine(d);
+//   d = true;
+//    System.Console.WriteLine(d);
+using System;
+
+class GFG
+{
+    public static void add(dynamic s1, dynamic s2)
+    {
+        Console.WriteLine(s1 + s2);
+    }
+    static public void Main()
+    {
+
+        add("hello", "world");
+        add("hii", 2.0);
+        add(2, 8);
+    }
+}
